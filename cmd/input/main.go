f966c8d70bef7ea8afe187d571dc34aefb90f8cf
Matt Burke <spraints@gmail.com>
2025-06-14 18:43:52 -0400

    Initial version of program
    cmd/input/main.go should be a program that lets me input a series of
    crop data points. I'd like it to use
    @https://github.com/charmbracelet/bubbletea for the UI. The output will
    look like this:
    
    cucumbers 60#
    squash, summer 88.8#
    strawberries 12 pt
    
    The input will be me typing a crop name (e.g. "cucumber") and the UI
    matching that against a list of crops in crops.txt. It should show a few
    matches, and then I can use the arrow keys to select the one I want or
    keep typing and press enter to add a new one. Then I type the quantity
    (e.g. 60#, 88.8 lb, 12 pt). When I'm done, I type Ctrl-D and it writes
    to an output file provided on the command line. Command line args will
    be like this: "go run ./cmd/input --crops FILE --out FILE"

---
+++ cmd/input/main.go
@@ -0,0 +1,201 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var (
+	cropsFile = flag.String("crops", "crops.txt", "path to crops file")
+	outFile   = flag.String("out", "", "path to output file")
+)
+
+type model struct {
+	crops          []string
+	filteredCrops  []string
+	cropInput      textinput.Model
+	quantityInput  textinput.Model
+	selectedCrop   string
+	selectedIndex  int
+	state          string // "crop" or "quantity"
+	output         []string
+}
+
+func initialModel(crops []string) model {
+	ti := textinput.New()
+	ti.Placeholder = "Type crop name..."
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 50
+
+	qi := textinput.New()
+	qi.Placeholder = "Type quantity (e.g. 60#, 88.8 lb, 12 pt)..."
+	qi.CharLimit = 156
+	qi.Width = 50
+
+	return model{
+		crops:         crops,
+		filteredCrops: crops,
+		cropInput:     ti,
+		quantityInput: qi,
+		state:         "crop",
+		output:        make([]string, 0),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "ctrl+d":
+			if m.state == "quantity" {
+				// Write output to file
+				if *outFile != "" {
+					f, err := os.Create(*outFile)
+					if err != nil {
+						fmt.Printf("Error creating output file: %v\n", err)
+						os.Exit(1)
+					}
+					defer f.Close()
+					for _, line := range m.output {
+						fmt.Fprintln(f, line)
+					}
+				}
+			}
+			return m, tea.Quit
+		}
+
+		if m.state == "crop" {
+			switch msg.String() {
+			case "enter":
+				if len(m.filteredCrops) > 0 {
+					m.selectedCrop = m.filteredCrops[m.selectedIndex]
+					m.state = "quantity"
+					m.quantityInput.Focus()
+					m.cropInput.Blur()
+				} else if m.cropInput.Value() != "" {
+					m.selectedCrop = m.cropInput.Value()
+					m.state = "quantity"
+					m.quantityInput.Focus()
+					m.cropInput.Blur()
+				}
+			case "up":
+				if m.selectedIndex > 0 {
+					m.selectedIndex--
+				}
+			case "down":
+				if m.selectedIndex < len(m.filteredCrops)-1 {
+					m.selectedIndex++
+				}
+			}
+		} else if m.state == "quantity" {
+			switch msg.String() {
+			case "enter":
+				if m.quantityInput.Value() != "" {
+					m.output = append(m.output, fmt.Sprintf("%s %s", m.selectedCrop, m.quantityInput.Value()))
+					m.state = "crop"
+					m.cropInput.Focus()
+					m.quantityInput.Blur()
+					m.quantityInput.Reset()
+					m.selectedCrop = ""
+					m.selectedIndex = 0
+				}
+			}
+		}
+	}
+
+	if m.state == "crop" {
+		m.cropInput, cmd = m.cropInput.Update(msg)
+		if m.cropInput.Value() != "" {
+			m.filteredCrops = make([]string, 0)
+			for _, crop := range m.crops {
+				if strings.Contains(strings.ToLower(crop), strings.ToLower(m.cropInput.Value())) {
+					m.filteredCrops = append(m.filteredCrops, crop)
+				}
+			}
+			if m.selectedIndex >= len(m.filteredCrops) {
+				m.selectedIndex = 0
+			}
+		} else {
+			m.filteredCrops = m.crops
+		}
+	} else {
+		m.quantityInput, cmd = m.quantityInput.Update(msg)
+	}
+
+	return m, cmd
+}
+
+func (m model) View() string {
+	var s strings.Builder
+
+	s.WriteString("Harvest Input\n\n")
+
+	if m.state == "crop" {
+		s.WriteString(m.cropInput.View())
+		s.WriteString("\n\n")
+		s.WriteString("Matches:\n")
+		for i, crop := range m.filteredCrops {
+			if i == m.selectedIndex {
+				s.WriteString("> ")
+			} else {
+				s.WriteString("  ")
+			}
+			s.WriteString(crop)
+			s.WriteString("\n")
+		}
+	} else {
+		s.WriteString(fmt.Sprintf("Selected crop: %s\n", m.selectedCrop))
+		s.WriteString(m.quantityInput.View())
+	}
+
+	s.WriteString("\n\n")
+	s.WriteString("Press Ctrl+D when done\n")
+
+	return s.String()
+}
+
+func main() {
+	flag.Parse()
+
+	if *outFile == "" {
+		fmt.Println("Error: --out flag is required")
+		os.Exit(1)
+	}
+
+	// Read crops file
+	file, err := os.Open(*cropsFile)
+	if err != nil {
+		fmt.Printf("Error opening crops file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	var crops []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line != "" && !strings.HasPrefix(line, "Week") {
+			crops = append(crops, line)
+		}
+	}
+
+	p := tea.NewProgram(initialModel(crops))
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
